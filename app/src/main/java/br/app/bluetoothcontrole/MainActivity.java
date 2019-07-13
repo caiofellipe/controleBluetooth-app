ef2c046bf1c3b709bf9b442104e3c234f0766858
Caio Oliveira <caiofellipe80@gmail.com>
2019-07-13 01:33:32 -0300

    Configurando conexão com Bluetooth

--- app/src/main/java/br/app/bluetoothcontrole/MainActivity.java
+++ app/src/main/java/br/app/bluetoothcontrole/MainActivity.java
@@ -1,17 +1,23 @@
 package br.app.bluetoothcontrole;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements JoystickView.JoystickListener{
-    int SOLICITA_BLUETOOTH;
+    private static final int SOLICITA_BLUETOOTH = 1;
+    private static final int SOLICITA_CONEXAO = 2;
     BluetoothAdapter myBluetooth = null;
-
+    Button btnConectar;
+    boolean conexao = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements JoystickView.Joys
         JoystickView joystick = new JoystickView(this);
         setContentView(R.layout.activity_main);
 
+        btnConectar = (Button)findViewById(R.id.btnConectar);
         myBluetooth = BluetoothAdapter.getDefaultAdapter();
         if(myBluetooth == null){
             Toast.makeText(getApplicationContext(), "Seu dispositivo não possui Bluetooth", Toast.LENGTH_LONG).show();
@@ -26,7 +33,34 @@ public class MainActivity extends AppCompatActivity implements JoystickView.Joys
             Intent ativaBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(ativaBluetooth, SOLICITA_BLUETOOTH);
         }
+        btnConectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(conexao){
+
+                }else{
+                    Intent lista = new Intent(MainActivity.this, ListDispositivos.class);
+                    startActivityForResult(lista, SOLICITA_CONEXAO);
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case SOLICITA_BLUETOOTH:
+                if(resultCode == Activity.RESULT_OK){
+                    Toast.makeText(getApplicationContext(), "Bluetooth ativado", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "O Bluetooth não foi ativado! O app foi encerrado", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            break;
+        }
+    }
+
     public void onJoystickMoved(float xPercent, float yPercent, int id){
         Log.d("Direita Joystick", "X Percent: " + xPercent + " Y Percent: " + yPercent);
     }
